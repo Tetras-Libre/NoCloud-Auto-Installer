b6d2ccea1460ba1475508db3333aed3f81ebbdac
David Beniamine <david.beniamine@imag.fr>
2017-01-23 10:55:55 +0100

    upgrade Dolibarr

---
+++ upgradeDolibarr.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DIR=`dirname $0`
+git pull
+current_version=`apt-cache policy dolibarr | grep Installed | awk '{print $2}'`
+echo "Current version of dolibarr: $current_version"
+package=`DOLIBARR_PACKAGES/*.deb`
+last_repo_version=`echo $package | sed 's/.*dolibarr_\([^_]*\)_.*/\1/'`
+if [ "$current_version" == "$last_repo_version" ]
+then
+    echo "Current version of dolibarr is the last version in our repo, nothing to do"
+    exit
+fi
+echo "Installing $package"
+dpkg -i $package
+echo "Fixing dependencies"
+apt-get -f install
+echo "You should now be running Dolibarr version $current_version"
+echo "Please check Dolibar webpage for possible manual upgrade steps" 

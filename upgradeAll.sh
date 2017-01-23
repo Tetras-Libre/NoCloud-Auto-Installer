9256ce40a47a7c0b251d375a97d71ba2bb228401
David Beniamine <david.beniamine@imag.fr>
2017-01-23 16:32:03 +0100

    aptitude upgrade in upgradeAll

--- upgradeAll.sh
+++ upgradeAll.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+aptitude update && aptitude upgrade
 DIR=`dirname $0`
 . $DIR/installNextcloud.env
 . $DIR/upgradeNextcloud.sh
 . $DIR/upgradeTetrasBack.sh
 . $DIR/upgradeDolibarr.sh
+checkrestart

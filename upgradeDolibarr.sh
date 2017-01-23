ff9f29c1cf554d3a64569fc5cb274124895261f0
David Beniamine <david.beniamine@imag.fr>
2017-01-23 11:13:09 +0100

    Fix errors on upgradeDolibarr

--- upgradeDolibarr.sh
+++ upgradeDolibarr.sh
@@ -3,11 +3,12 @@ DIR=`dirname $0`
 git pull
 current_version=`apt-cache policy dolibarr | grep Installed | awk '{print $2}'`
 echo "Current version of dolibarr: $current_version"
-package=`DOLIBARR_PACKAGES/*.deb`
+package=`/bin/ls DOLIBARR_PACKAGES/*.deb`
 last_repo_version=`echo $package | sed 's/.*dolibarr_\([^_]*\)_.*/\1/'`
+echo "Version of dolibarri on our repo: $current_version"
 if [ "$current_version" == "$last_repo_version" ]
 then
-    echo "Current version of dolibarr is the last version in our repo, nothing to do"
+echo "Current version of dolibarr is the last version in our repo, nothing to do"
     exit
 fi
 echo "Installing $package"
@@ -15,4 +16,4 @@ dpkg -i $package
 echo "Fixing dependencies"
 apt-get -f install
 echo "You should now be running Dolibarr version $current_version"
-echo "Please check Dolibar webpage for possible manual upgrade steps" 
+echo "Please check Dolibar webpage for possible manual upgrade steps"

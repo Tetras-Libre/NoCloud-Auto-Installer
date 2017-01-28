9a94008c08e2d342de9d734793cac977d58833a5
David Beniamine <david.beniamine@imag.fr>
2017-01-28 18:40:57 +0100

    Enable tetras-back apache config

--- installTetrasBack.sh
+++ installTetrasBack.sh
@@ -16,6 +16,10 @@
 # You should have received a copy of the GNU General Public License # along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+TB_CONFIG_ServerName=${DOLIBARR_CONFIG_ServerName:-tetras-back.${DOMAIN}}
+TB_CONFIG_ServerAdmin=${DOLIBARR_CONFIG_ServerAdmin:-${SERVER_ADMIN}}
+
 set -o nounset                              # Treat unset variables as an error
 
 DEBIAN_FRONTEND="noninteractive" apt-get -qq install git gnulib perl sendmail \
@@ -32,3 +36,43 @@ make
 unset DEBIAN_FRONTEND
 
 cd $OriginalDirectory
+
+echo "Set tetras-back's configuration file for apache 2"
+
+# Save last tetras-back-ssh.conf if exists
+if [ -f /etc/apache2/sites-available/tetras-back-ssl.conf ]
+then
+    echo "Dolibarr's apache configuration already exists"
+    echo "Backup file is created at" \
+        "/etc/apache2/sites-available/${RUNNING_DATE_TIME}_tetras-back-ssl.conf"
+
+    echo "cp ${VERBOSE:+-v} /etc/apache2/sites-available/tetras-back-ssl.conf" \
+        "/etc/apache2/sites-available/${RUNNING_DATE_TIME}_tetras-back-ssl.conf"
+    cp ${VERBOSE:+-v} /etc/apache2/sites-available/tetras-back-ssl.conf \
+    /etc/apache2/sites-available/${RUNNING_DATE_TIME}_tetras-back-ssl.conf
+fi
+sed "s@<+ServerAdmin+>@${DOLIBARR_CONFIG_ServerAdmin:-<+ServerAdmin+>}@;
+    s@<+ServerName+>@${DOLIBARR_CONFIG_ServerName:-<+ServerName+>}@" \
+        ${SCRIPT_DIRECTORY%%/}/template_tetras-back-ssl.conf > \
+    /etc/apache2/sites-available/tetras-back-ssl.conf
+
+# Set ssl.conf
+if [ -f /etc/apache2/sites-available/ssl.conf ]
+then
+    echo "Apache ssl configuration already exists"
+    echo "Backup file is created at " \
+        "/etc/apache2/sites-available/${RUNNING_DATE_TIME}-ssl.conf"
+
+    echo "cp ${VERBOSE:+-v} /etc/apache2/sites-available/tetras-back-ssl.conf" \
+    "/etc/apache2/sites-available/${RUNNING_DATE_TIME}-ssl.conf"
+    cp ${VERBOSE:+-v} /etc/apache2/ssl.conf \
+    /etc/apache2/${RUNNING_DATE_TIME}-ssl.conf
+fi
+sed \
+    "s@<+SSLCertificateFile+>@${NEXTCLOUD_CONFIG_certificateFile:-<+SSLCertificateFile+>}@
+    s@<+SSLCertificateKeyFile+>@${NEXTCLOUD_CONFIG_certificateKeyFile:-<+SSLCertificateKeyFile+>}@" \
+        ${SCRIPT_DIRECTORY%%/}/template_ssl.conf > \
+        /etc/apache2/ssl.conf
+
+a2ensite tetras-back-ssl.conf
+apachectl configtest && apachectl restart || echo "Failed restartin apache"

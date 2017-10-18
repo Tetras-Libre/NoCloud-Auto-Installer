61ffd135f8422273fef1890ea3c8cb384fff8001
David Beniamine <david.beniamine@tetras-libre.fr>
2017-10-18 10:59:56 +0200

    patch not required for DOLIBARR >= 6.0.1

--- DOLIBARR_PACKAGES/apply_patch.sh
+++ DOLIBARR_PACKAGES/apply_patch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Fixing Dolibarr bug #7420"
-sed -i.bak "178s@'\(__[^_]*__\)'@'/\1/'@g"  /usr/share/dolibarr/htdocs/includes/odtphp/Segment.php
+# echo "Fixing Dolibarr bug #7420"
+# sed -i.bak "178s@'\(__[^_]*__\)'@'/\1/'@g"  /usr/share/dolibarr/htdocs/includes/odtphp/Segment.php
